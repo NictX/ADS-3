b7e0c00edfa001c68c7004590dafbc724c7f778a
Nict <71667497+NictX@users.noreply.github.com>
2022-03-13 17:35:32 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,26 +1,26 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int left = 0;
-  int right = size - 1;
+  int l = 0;
+  int r = size - 1;
   int count = 0;
-  while (right >= left) {
-    if (arr[(left + right) / 2] == value) {
+  while (r >= l) {
+    if (arr[(l + r) / 2] == value) {
       count++;
       break;
     }
-    if (arr[(left + right) / 2] < value) {
-      left = (left + right) / 2 + 1;
+    if (arr[(l + r) / 2] < value) {
+      l = (l + r) / 2 + 1;
     }
-    if (arr[(left + right) / 2] > value) {
-      right = (left + right) / 2 - 1;
+    if (arr[(l + r) / 2] > value) {
+      r = (l + r) / 2 - 1;
     }
   }
   if (count != 0) {
-    for (int i = (left + right) / 2 + 1; arr[i] == value && i <= size - 1; i++) {
+    for (int i = (l + r) / 2 + 1; arr[i] == value && i <= size - 1; i++) {
       count++;
     }
-    for (int i = (left + right) / 2 - 1; arr[i] == value && i >= 0; i--) {
+    for (int i = (l + r) / 2 - 1; arr[i] == value && i >= 0; i--) {
       count++;
     }
   }
